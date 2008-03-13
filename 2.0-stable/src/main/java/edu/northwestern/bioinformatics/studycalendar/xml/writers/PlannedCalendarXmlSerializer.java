5776e742896a5149fa7afadd7353659acb34d319
John Dzak <j-dzak@northwestern.edu>
2008-03-13 22:05:09 +0000

    Back-ported addition of the resource to get a realized template from a certain amendment from the trunk.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/branches/releases/phase2@2534 0d517254-b314-0410-acde-c619094fa49f

--- 2.0-stable/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedCalendarXmlSerializer.java
+++ 2.0-stable/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedCalendarXmlSerializer.java
@@ -1,56 +1,80 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
 import edu.northwestern.bioinformatics.studycalendar.dao.PlannedCalendarDao;
-import edu.northwestern.bioinformatics.studycalendar.domain.PlanTreeInnerNode;
-import edu.northwestern.bioinformatics.studycalendar.domain.PlanTreeNode;
+import edu.northwestern.bioinformatics.studycalendar.domain.Epoch;
 import edu.northwestern.bioinformatics.studycalendar.domain.PlannedCalendar;
+import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
 import org.dom4j.Element;
+import org.springframework.beans.factory.annotation.Required;
 
-public class PlannedCalendarXmlSerializer extends AbstractPlanTreeNodeXmlSerializer {
+import java.util.List;
+
+public class PlannedCalendarXmlSerializer extends AbstractStudyCalendarXmlSerializer <PlannedCalendar> {
     public static final String PLANNED_CALENDAR = "planned-calendar";
     private PlannedCalendarDao plannedCalendarDao;
     private boolean serializeEpoch;
+    private Study study;
 
     public PlannedCalendarXmlSerializer() {
         serializeEpoch = false;
     }
 
-    protected PlanTreeNode<?> nodeInstance() {
-        return new PlannedCalendar();
-    }
+    public Element createElement(PlannedCalendar cal) {
+        Element element = element(PLANNED_CALENDAR);
+        element.addAttribute(ID, cal.getGridId());
 
-    protected String elementName() {
-        return PLANNED_CALENDAR;
-    }
-
-    protected PlanTreeNode<?> getFromId(String id) {
-        return plannedCalendarDao.getByGridId(id);
-    }
-
-    protected AbstractPlanTreeNodeXmlSerializer getChildSerializer() {
         if (serializeEpoch) {
-            EpochXmlSerializer serializer = (EpochXmlSerializer) getBeanFactory().getBean("epochXmlSerializer");
-            serializer.setStudy(study);
-            return serializer;
-        }
-        return null;
-    }
-    
-    protected void addChildrenElements(PlanTreeInnerNode<?, PlanTreeNode<?>, ?> node, Element eStudySegment) {
-        if (getChildSerializer() != null) {
-            for (PlanTreeNode<?> oChildNode : node.getChildren()) {
-                Element childElement = getChildSerializer().createElement(oChildNode);
-                eStudySegment.add(childElement);
+            EpochXmlSerializer serializer = getEpochSerializer();
+
+            for (Epoch epoch : cal.getEpochs()) {
+                element.add(serializer.createElement(epoch));
             }
         }
+
+        return element;
     }
 
+    @SuppressWarnings({"unchecked"})
+    public PlannedCalendar readElement(Element element) {
+        if (!element.getName().equals(PLANNED_CALENDAR)) {
+            return null;
+        }
+
+        String key = element.attributeValue(ID);
+        PlannedCalendar cal = plannedCalendarDao.getByGridId(key);
+        if (cal == null) {
+            cal = new PlannedCalendar();
+            cal.setGridId(key);
+
+            EpochXmlSerializer serializer = getEpochSerializer();
+            
+            if (serializer != null) {
+                for (Element elt : (List<Element>) element.elements()) {
+                    cal.addEpoch((Epoch) serializer.readElement(elt));
+                }
+            }
+        }
+        return cal;
+    }
+
+    protected EpochXmlSerializer getEpochSerializer() {
+        EpochXmlSerializer serializer = (EpochXmlSerializer) getBeanFactory().getBean("epochXmlSerializer");
+        serializer.setStudy(study);
+        return serializer;
+    }
+
+    public void setSerializeEpoch(boolean serializeEpoch) {
+        this.serializeEpoch = serializeEpoch;
+    }
+
+    ////// Bean Setters
+    @Required
     public void setPlannedCalendarDao(PlannedCalendarDao plannedCalendarDao) {
         this.plannedCalendarDao = plannedCalendarDao;
     }
 
-
-    public void setSerializeEpoch(boolean serializeEpoch) {
-        this.serializeEpoch = serializeEpoch;
+    public void setStudy(Study study) {
+        this.study = study;
     }
 }
