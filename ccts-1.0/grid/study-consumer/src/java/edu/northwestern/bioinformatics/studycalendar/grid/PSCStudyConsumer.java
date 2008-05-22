e590714d41060633bce5e3ca290b3ed7b22c6ee5
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-05-22 16:02:41 +0000

    removing DOCTYPE from xml files. Otherwise loading application context fails on linux machine. Adding readOnly=false attribute to AdverseEventConsumerGridService.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/branches/releases/phase2@2757 0d517254-b314-0410-acde-c619094fa49f

--- ccts-1.0/grid/study-consumer/src/java/edu/northwestern/bioinformatics/studycalendar/grid/PSCStudyConsumer.java
+++ ccts-1.0/grid/study-consumer/src/java/edu/northwestern/bioinformatics/studycalendar/grid/PSCStudyConsumer.java
@@ -35,7 +35,8 @@ import java.util.List;
  */
 
 @Transactional(readOnly = false)
-public class PSCStudyConsumer implements StudyConsumerI {
+public class
+        PSCStudyConsumer implements StudyConsumerI {
 
     private static final Log logger = LogFactory.getLog(PSCStudyConsumer.class);
 
@@ -209,13 +210,14 @@ public class PSCStudyConsumer implements StudyConsumerI {
                 }
                 studySites.add(studySite);
             }
+        }
             if (studySites.size() == 0 || ArrayUtils.isEmpty(studyOrganizationTypes)) {
                 String message = "No sites is associated to this study" + study.getLongTitle();
                 throw getStudyCreationException(message);
 
             }
             study.setStudySites(studySites);
-        }
+
     }
 
     /**
