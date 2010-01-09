NUBIC/psc-mirror

commit 3d64551fab896ce2b6eed0a3f5af3ad06b55b638

Fail psc:dist (with an explanatory message) if the oracle driver isn't present.  Fixes #896.
git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/branches/releases/2.6-stable@4758 0d517254-b314-0410-acde-c619094fa49f

buildfile
@@ -1200,8 +1200,14 @@ define "psc" do
 
     dist_dir = "target/dist/bin"
     mkdir_p _("#{dist_dir}/conf-samples")
-    cp _("db/datasource.properties.example"), _("#{dist_dir}/conf-samples/datasource.properties")
+
     cp project('web').packages.select { |p| p.type == :war }.to_s, _("#{dist_dir}/psc.war")
+    # Ensure oracle driver is present in war
+    unless `jar tf #{_(dist_dir, 'psc.war')}` =~ /ojdbc/
+      fail "Oracle JDBC driver not present in war.  Distributions must be built with ORACLE=yes."
+    end
+
+    cp _("db/datasource.properties.example"), _("#{dist_dir}/conf-samples/datasource.properties")
     puts `svn export https://ncisvn.nci.nih.gov/svn/psc/documents/PSC_Install_Guide.doc #{_("#{dist_dir}/psc_install.doc")}`
 
     pkg_name = "psc-#{VERSION_NUMBER.sub(/.RELEASE/, '')}"

tasks/helper_functions.rake
@@ -54,7 +54,7 @@ def db_deps
   [
     DB.postgresql,
     DB.hsqldb,
-    (DB.oracle if ENV['ORACLE'])
+    (DB.oracle if env_true?('ORACLE'))
   ].compact
 end
 

commit 5fea5cd11e9d13971fb27a0a2d88ea3c33e41c75

Fix another quoting problem introduced in r4758.  #896.
git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/branches/releases/2.6-stable@4762 0d517254-b314-0410-acde-c619094fa49f

buildfile
@@ -1208,7 +1208,7 @@ define "psc" do
     end
 
     cp _("db/datasource.properties.example"), _("#{dist_dir}/conf-samples/datasource.properties")
-    puts `svn export https://ncisvn.nci.nih.gov/svn/psc/documents/PSC_Install_Guide.doc #{_("#{dist_dir}/psc_install.doc")}`
+    puts `svn export https://ncisvn.nci.nih.gov/svn/psc/documents/PSC_Install_Guide.doc '#{_("#{dist_dir}/psc_install.doc")}'`
 
     pkg_name = "psc-#{VERSION_NUMBER.sub(/.RELEASE/, '')}"
 