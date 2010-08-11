NUBIC/psc-mirror

commit 19e604dc849b1a418273421058abd3a658e91bc5

Refactor base provisioning command so that it can be used for provisioning multiple users at once.  #1105.
(Needed for team-by-study administration.)

git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/trunk@5417 0d517254-b314-0410-acde-c619094fa49f

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/AdministerUserController.java
@@ -6,7 +6,7 @@ import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
 import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole;
 import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscUser;
 import edu.northwestern.bioinformatics.studycalendar.service.PscUserService;
-import edu.northwestern.bioinformatics.studycalendar.utils.editors.JsonArrayEditor;
+import edu.northwestern.bioinformatics.studycalendar.utils.editors.JsonObjectEditor;
 import edu.northwestern.bioinformatics.studycalendar.web.PscAbstractCommandController;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.PscAuthorizedHandler;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.ResourceAuthorization;
@@ -14,7 +14,7 @@ import edu.northwestern.bioinformatics.studycalendar.web.osgi.InstalledAuthentic
 import edu.nwu.bioinformatics.commons.spring.ValidatableValidator;
 import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSessionFactory;
 import gov.nih.nci.security.AuthorizationManager;
-import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.ServletRequestDataBinder;
@@ -78,7 +78,7 @@ public class AdministerUserController
         HttpServletRequest request, ServletRequestDataBinder binder
     ) throws Exception {
         super.initBinder(request, binder);
-        binder.registerCustomEditor(JSONArray.class, "roleChanges", new JsonArrayEditor());
+        binder.registerCustomEditor(JSONObject.class, "roleChanges", new JsonObjectEditor());
         binder.registerCustomEditor(
             Date.class, "user.csmUser.endDate", getControllerTools().getDateEditor(false));
     }

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/utils/editors/JsonObjectEditor.java
@@ -1,0 +1,20 @@
+package edu.northwestern.bioinformatics.studycalendar.utils.editors;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class JsonObjectEditor extends PropertyEditorSupport {
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        try {
+            setValue(new JSONObject(text));
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("\"" + text + "\" is not parseable as JSON.", e);
+        }
+    }
+}

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/BaseUserProvisioningCommand.java
@@ -14,7 +14,6 @@ import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSessionFactory;
 import gov.nih.nci.cabig.ctms.suite.authorization.ScopeType;
 import gov.nih.nci.cabig.ctms.suite.authorization.SuiteRole;
 import gov.nih.nci.cabig.ctms.suite.authorization.SuiteRoleMembership;
-import gov.nih.nci.security.exceptions.CSTransactionException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,9 +21,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -36,7 +38,7 @@ import static edu.northwestern.bioinformatics.studycalendar.core.accesscontrol.A
 /**
  * @author Rhett Sutphin
  */
-public abstract class AbstractSingleUserProvisioningCommand {
+public abstract class BaseUserProvisioningCommand {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     public static final String JSON_CHANGE_PROP_ROLE = "role";
@@ -45,10 +47,9 @@ public abstract class AbstractSingleUserProvisioningCommand {
     public static final String JSON_CHANGE_PROP_SCOPE_IDENTIFIER = "scopeIdentifier";
     public static final String JSON_ALL_SCOPE_IDENTIFIER = "__ALL__";
 
-    protected PscUser user;
-    private JSONArray roleChanges;
+    private List<PscUser> users;
+    private JSONObject roleChanges;
 
-    private ProvisioningSession provisioningSession;
     private final ProvisioningSessionFactory provisioningSessionFactory;
     private final ApplicationSecurityManager applicationSecurityManager;
 
@@ -64,12 +65,20 @@ public abstract class AbstractSingleUserProvisioningCommand {
     private Set<String> provisionableParticipatingStudyIdentifiers;
     protected static final int JSON_INDENT_DEPTH = 4;
 
-    public AbstractSingleUserProvisioningCommand(
+    protected BaseUserProvisioningCommand(
         PscUser user,
         ProvisioningSessionFactory provisioningSessionFactory,
         ApplicationSecurityManager applicationSecurityManager
     ) {
-        this.user = user;
+        this(Arrays.asList(user), provisioningSessionFactory, applicationSecurityManager);
+    }
+
+    protected BaseUserProvisioningCommand(
+        List<PscUser> users,
+        ProvisioningSessionFactory provisioningSessionFactory,
+        ApplicationSecurityManager applicationSecurityManager
+    ) {
+        this.users = users;
         this.applicationSecurityManager = applicationSecurityManager;
         this.provisioningSessionFactory = provisioningSessionFactory;
 
@@ -85,17 +94,26 @@ public abstract class AbstractSingleUserProvisioningCommand {
 
     ////// APPLY
 
-    public void apply() throws CSTransactionException {
-        Map<String, List<SubmittedChange>> changesByType = classifyAndFilterChanges();
-        applyAddAndRemoveScopes(changesByType.get("specificScope"));
-        applyAddAndRemoveAllScope(changesByType.get("allScope"));
-        applyAddAndRemoveGroupOnly(changesByType.get("groupOnly"));
-        applyStaleFlag();
+    public void apply() throws Exception {
+        Map<PscUser, Map<String, List<SubmittedChange>>> changesByUserAndType
+            = classifyAndFilterChanges();
+        for (Map.Entry<PscUser, Map<String, List<SubmittedChange>>> entry : changesByUserAndType.entrySet()) {
+            PscUser user = entry.getKey();
+            ProvisioningSession session = provisioningSessionFactory.createSession(
+                user.getCsmUser().getUserId());
+            applyAddAndRemoveScopes(session, entry.getValue().get("specificScope"));
+            applyAddAndRemoveAllScope(session, entry.getValue().get("allScope"));
+            applyAddAndRemoveGroupOnly(session, entry.getValue().get("groupOnly"));
+        }
+        // apply separately since non-role changes may have occurred
+        for (PscUser user : getUsers()) {
+            applyStaleFlag(user);
+        }
     }
 
-    private void applyAddAndRemoveScopes(List<SubmittedChange> specificScopeChanges) {
+    private void applyAddAndRemoveScopes(ProvisioningSession session, List<SubmittedChange> specificScopeChanges) {
         for (SubmittedChange change : specificScopeChanges) {
-            SuiteRoleMembership base = getProvisioningSession().getProvisionableRoleMembership(change.getRole());
+            SuiteRoleMembership base = session.getProvisionableRoleMembership(change.getRole());
             if (change.isAdd()) {
                 if (change.getScopeType() == ScopeType.SITE) base.addSite(change.getScopeIdentifier());
                 else base.addStudy(change.getScopeIdentifier());
@@ -104,13 +122,13 @@ public abstract class AbstractSingleUserProvisioningCommand {
                 else base.removeStudy(change.getScopeIdentifier());
             }
 
-            getProvisioningSession().replaceRole(base);
+            session.replaceRole(base);
         }
     }
 
-    private void applyAddAndRemoveAllScope(List<SubmittedChange> allScopeChanges) {
+    private void applyAddAndRemoveAllScope(ProvisioningSession session, List<SubmittedChange> allScopeChanges) {
         for (SubmittedChange change : allScopeChanges) {
-            SuiteRoleMembership base = getProvisioningSession().getProvisionableRoleMembership(change.getRole());
+            SuiteRoleMembership base = session.getProvisionableRoleMembership(change.getRole());
             if (change.isAdd()) {
                 if (change.getScopeType() == ScopeType.SITE) base.forAllSites();
                 else base.forAllStudies();
@@ -119,61 +137,73 @@ public abstract class AbstractSingleUserProvisioningCommand {
                 else base.notForAllStudies();
             }
 
-            getProvisioningSession().replaceRole(base);
+            session.replaceRole(base);
         }
     }
 
-    private void applyAddAndRemoveGroupOnly(List<SubmittedChange> groupOnlyChanges) {
+    private void applyAddAndRemoveGroupOnly(ProvisioningSession session, List<SubmittedChange> groupOnlyChanges) {
         for (SubmittedChange change : groupOnlyChanges) {
             if (change.isAdd()) {
-                getProvisioningSession().replaceRole(
-                    getProvisioningSession().getProvisionableRoleMembership(change.getRole()));
+                session.replaceRole(
+                    session.getProvisionableRoleMembership(change.getRole()));
             } else if (change.isRemove()) {
-                getProvisioningSession().deleteRole(change.getRole());
+                session.deleteRole(change.getRole());
             }
         }
     }
 
-    private void applyStaleFlag() {
+    private void applyStaleFlag(PscUser user) {
         if (applicationSecurityManager == null) return;
         PscUser principal = applicationSecurityManager.getUser();
-        if (principal.getCsmUser().getUserId().equals(getUser().getCsmUser().getUserId())) {
+        if (principal.getCsmUser().getUserId().equals(user.getCsmUser().getUserId())) {
             principal.setStale(true);
         }
     }
 
-    private ProvisioningSession getProvisioningSession() {
-        if (provisioningSession == null) {
-            provisioningSession =
-                provisioningSessionFactory.createSession(getUser().getCsmUser().getUserId());
+    @SuppressWarnings({"unchecked"})
+    private Map<PscUser, Map<String, List<SubmittedChange>>> classifyAndFilterChanges() {
+        Map<PscUser, Map<String, List<SubmittedChange>>> byUser =
+            new LinkedHashMap<PscUser, Map<String, List<SubmittedChange>>>();
+        for (Iterator<String> keys = getRoleChanges().keys(); keys.hasNext();) {
+            String username = keys.next();
+            PscUser user = findUser(username);
+            if (user == null) {
+                log.warn("Ignoring unauthorized attempt to change roles for {}", username);
+                continue;
+            }
+            Map<String, List<SubmittedChange>> classified = new MapBuilder<String, List<SubmittedChange>>().
+                put("specificScope", new LinkedList<SubmittedChange>()).
+                put("allScope", new LinkedList<SubmittedChange>()).
+                put("groupOnly", new LinkedList<SubmittedChange>()).
+                toMap();
+            JSONArray userRoleChanges = getRoleChanges().optJSONArray(username);
+            for (int i = 0; i < userRoleChanges.length(); i++) {
+                try {
+                    SubmittedChange change = new SubmittedChange(userRoleChanges.getJSONObject(i));
+                    if (shouldSkip(change)) continue;
+
+                    if (!change.isScopeChange()) {
+                        classified.get("groupOnly").add(change);
+                    } else if (change.isAllScope()) {
+                        classified.get("allScope").add(change);
+                    } else {
+                        classified.get("specificScope").add(change);
+                    }
+                } catch (JSONException e) {
+                    throw new StudyCalendarValidationException(
+                        "One of the elements in the change list isn't an object or is otherwise invalid", e);
+                }
+            }
+            byUser.put(user, classified);
         }
-        return provisioningSession;
+        return byUser;
     }
 
-    private Map<String, List<SubmittedChange>> classifyAndFilterChanges() {
-        Map<String, List<SubmittedChange>> classified = new MapBuilder<String, List<SubmittedChange>>().
-            put("specificScope", new LinkedList<SubmittedChange>()).
-            put("allScope", new LinkedList<SubmittedChange>()).
-            put("groupOnly", new LinkedList<SubmittedChange>()).
-            toMap();
-        for (int i = 0; i < getRoleChanges().length(); i++) {
-            try {
-                SubmittedChange change = new SubmittedChange(getRoleChanges().getJSONObject(i));
-                if (shouldSkip(change)) continue;
-
-                if (!change.isScopeChange()) {
-                    classified.get("groupOnly").add(change);
-                } else if (change.isAllScope()) {
-                    classified.get("allScope").add(change);
-                } else {
-                    classified.get("specificScope").add(change);
-                }
-            } catch (JSONException e) {
-                throw new StudyCalendarValidationException(
-                    "One of the elements in the change list isn't an object or is otherwise invalid", e);
-            }
+    private PscUser findUser(String username) {
+        for (PscUser user : getUsers()) {
+            if (user.getUsername().equals(username)) return user;
         }
-        return classified;
+        return null;
     }
 
     private boolean shouldSkip(SubmittedChange change) {
@@ -220,26 +250,26 @@ public abstract class AbstractSingleUserProvisioningCommand {
         return false;
     }
 
-    public Map<SuiteRole, SuiteRoleMembership> getCurrentRoles() {
-        return user.getMemberships();
-    }
-
     ////// JAVASCRIPT SERIALIZATION
 
     public String getJavaScriptProvisionableUser() {
+        return buildJavaScriptProvisionableUser(getUser());
+    }
+
+    private String buildJavaScriptProvisionableUser(PscUser user) {
         try {
             return String.format(
                 "new psc.admin.ProvisionableUser('%s', %s)",
-                getUser().getCsmUser().getLoginName(),
-                buildProvisionableUserRoleJSON().toString(JSON_INDENT_DEPTH));
+                user.getCsmUser().getLoginName(),
+                buildProvisionableUserRoleJSON(user).toString(JSON_INDENT_DEPTH));
         } catch (JSONException e) {
             throw new StudyCalendarSystemException("Building JSON for provisionable user failed", e);
         }
     }
 
-    private JSONObject buildProvisionableUserRoleJSON() throws JSONException {
+    private JSONObject buildProvisionableUserRoleJSON(PscUser user) throws JSONException {
         JSONObject rolesJSON = new JSONObject();
-        for (Map.Entry<SuiteRole, SuiteRoleMembership> entry : getCurrentRoles().entrySet()) {
+        for (Map.Entry<SuiteRole, SuiteRoleMembership> entry : user.getMemberships().entrySet()) {
             rolesJSON.put(entry.getKey().getCsmName(), buildProvisionableUserScopeJSON(entry.getValue()));
         }
         return rolesJSON;
@@ -262,6 +292,22 @@ public abstract class AbstractSingleUserProvisioningCommand {
     }
 
     ////// CONFIGURATION
+
+    public List<PscUser> getUsers() {
+        return users;
+    }
+
+    /**
+     * Returns the first configured user.
+     */
+    public PscUser getUser() {
+        return getUsers().isEmpty() ? null : getUsers().get(0);
+    }
+
+    // internal use only
+    protected void setUser(PscUser replacement) {
+        this.users = Arrays.asList(replacement);
+    }
 
     public List<ProvisioningRole> getProvisionableRoles() {
         return provisionableRoles;
@@ -344,19 +390,15 @@ public abstract class AbstractSingleUserProvisioningCommand {
 
     ////// BOUND PROPERTIES
 
-    public PscUser getUser() {
-        return user;
-    }
-
-    public JSONArray getRoleChanges() {
+    public JSONObject getRoleChanges() {
         if (roleChanges == null) {
-            roleChanges = new JSONArray();
+            roleChanges = new JSONObject();
         }
         return roleChanges;
     }
 
-    @SuppressWarnings({ "UnusedDeclaration" })
-    public void setRoleChanges(JSONArray roleChanges) {
+    @SuppressWarnings({ "UnusedDeclaration" }) // used in binding
+    public void setRoleChanges(JSONObject roleChanges) {
         this.roleChanges = roleChanges;
     }
 

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/ProvisionUserCommand.java
@@ -46,24 +46,24 @@ import java.util.Set;
  * @author Rhett Sutphin
  */
 public class ProvisionUserCommand
-    extends AbstractSingleUserProvisioningCommand
+    extends BaseUserProvisioningCommand
     implements Validatable
 {
-
     private final AuthorizationManager authorizationManager;
     private final AuthenticationSystem authenticationSystem;
 
     private boolean lookUpBoundUser;
     private String password, rePassword;
 
-    private ProvisionUserCommand(
+    protected ProvisionUserCommand(
         PscUser user,
         ProvisioningSessionFactory provisioningSessionFactory,
         AuthorizationManager authorizationManager,
         AuthenticationSystem authenticationSystem,
         ApplicationSecurityManager applicationSecurityManager
     ) {
-        super(user, provisioningSessionFactory, applicationSecurityManager);
+        super(user == null ? AuthorizationObjectFactory.createPscUser() : user,
+            provisioningSessionFactory, applicationSecurityManager);
         this.authorizationManager = authorizationManager;
         this.authenticationSystem = authenticationSystem;
     }
@@ -75,8 +75,7 @@ public class ProvisionUserCommand
         ApplicationSecurityManager applicationSecurityManager,
         SiteDao siteDao, StudyDao studyDao, PscUser provisioner
     ) {
-        ProvisionUserCommand command = new ProvisionUserCommand(
-            existingUser == null ? AuthorizationObjectFactory.createPscUser() : existingUser,
+        ProvisionUserCommand command = new ProvisionUserCommand(existingUser,
             psFactory, authorizationManager, authenticationSystem, applicationSecurityManager);
         if (provisioner == null) return command;
 
@@ -144,7 +143,7 @@ public class ProvisionUserCommand
     }
 
     @Override
-    public void apply() throws CSTransactionException {
+    public void apply() throws Exception {
         applyPassword();
         saveOrUpdateUser();
         super.apply();
@@ -172,7 +171,7 @@ public class ProvisionUserCommand
             User found = authorizationManager.getUser(getUser().getCsmUser().getLoginName());
             if (found != null) {
                 copyBoundProperties(this.getUser().getCsmUser(), found);
-                this.user = AuthorizationObjectFactory.createPscUser(found);
+                setUser(AuthorizationObjectFactory.createPscUser(found));
                 authorizationManager.modifyUser(getUser().getCsmUser());
             } else {
                 authorizationManager.createUser(getUser().getCsmUser());

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/SingleMemberStudyTeamMemberCommand.java
@@ -16,7 +16,7 @@ import java.util.TreeMap;
 /**
  * @author Rhett Sutphin
  */
-public class SingleMemberStudyTeamMemberCommand extends AbstractSingleUserProvisioningCommand {
+public class SingleMemberStudyTeamMemberCommand extends BaseUserProvisioningCommand {
     private Map<String, Map<PscRole, StudyTeamRoleMembership>> teamMemberships;
 
     private SingleMemberStudyTeamMemberCommand(

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/SingleMemberStudyTeamMemberController.java
@@ -5,11 +5,11 @@ import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
 import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole;
 import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscUser;
 import edu.northwestern.bioinformatics.studycalendar.service.PscUserService;
-import edu.northwestern.bioinformatics.studycalendar.utils.editors.JsonArrayEditor;
+import edu.northwestern.bioinformatics.studycalendar.utils.editors.JsonObjectEditor;
 import edu.northwestern.bioinformatics.studycalendar.web.PscAbstractCommandController;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.ResourceAuthorization;
 import gov.nih.nci.cabig.ctms.suite.authorization.ProvisioningSessionFactory;
-import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.ServletRequestDataBinder;
@@ -49,7 +49,7 @@ public class SingleMemberStudyTeamMemberController extends PscAbstractCommandCon
         HttpServletRequest request, ServletRequestDataBinder binder
     ) throws Exception {
         super.initBinder(request, binder);
-        binder.registerCustomEditor(JSONArray.class, "roleChanges", new JsonArrayEditor());
+        binder.registerCustomEditor(JSONObject.class, "roleChanges", new JsonObjectEditor());
     }
 
     @Override

commit 96bc6f6a00c2b5bdf02fea4ecc0663b3b2f211f0

Fix new-user validation problem introduced in r5417 or thereabouts.  #1055.
git-svn-id: https://ncisvn.nci.nih.gov/svn/psc/trunk@5442 0d517254-b314-0410-acde-c619094fa49f

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/AdministerUserCommand.java
@@ -108,24 +108,24 @@ public class AdministerUserCommand
 
     public void validate(Errors errors) {
         if (StringUtils.isBlank(getUser().getCsmUser().getLoginName())) {
-            errors.rejectValue("user.loginName", "error.user.name.not.specified");
+            errors.rejectValue("user.csmUser.loginName", "error.user.name.not.specified");
         } else {
             User existing = authorizationManager.getUser(getUser().getCsmUser().getLoginName());
             boolean existingMismatch = existing != null && !existing.getUserId().equals(getUser().getCsmUser().getUserId());
             if (!lookUpBoundUser && ((isNewUser() && existing != null) || existingMismatch)) {
-                errors.rejectValue("user.loginName", "error.user.name.already.exists");
+                errors.rejectValue("user.csmUser.loginName", "error.user.name.already.exists");
             }
         }
 
         if (StringUtils.isBlank(getUser().getCsmUser().getFirstName())) {
-            errors.rejectValue("user.firstName", "error.user.firstName.not.specified");
+            errors.rejectValue("user.csmUser.firstName", "error.user.firstName.not.specified");
         }
         if (StringUtils.isBlank(getUser().getCsmUser().getLastName())) {
-            errors.rejectValue("user.lastName", "error.user.lastName.not.specified");
+            errors.rejectValue("user.csmUser.lastName", "error.user.lastName.not.specified");
         }
 
         if (!GenericValidator.isEmail(getUser().getCsmUser().getEmailId())) {
-            errors.rejectValue("user.emailId", "error.user.email.invalid");
+            errors.rejectValue("user.csmUser.emailId", "error.user.email.invalid");
         }
 
         if (isNewUser() && getUsesLocalPasswords() && (StringUtils.isBlank(getPassword()))) {

web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/admin/AdministerUserController.java
@@ -98,7 +98,10 @@ public class AdministerUserController
             command.apply();
             return new ModelAndView("redirectToUserList");
         } else {
-            return new ModelAndView("admin/administerUser", errors.getModel());
+            ModelAndView mv = new ModelAndView("admin/administerUser", errors.getModel());
+            mv.addObject("startingNewUser", 
+                command.isNewUser() && "GET".equals(request.getMethod()));
+            return mv;
         }
     }
 

web/src/main/webapp/WEB-INF/views/admin/administerUser.jsp
@@ -4,7 +4,10 @@
 <%@taglib prefix="laf" tagdir="/WEB-INF/tags/laf"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
-<jsp:useBean id="command" scope="request" type="edu.northwestern.bioinformatics.studycalendar.web.admin.AdministerUserCommand"/>
+<jsp:useBean id="command" scope="request"
+             type="edu.northwestern.bioinformatics.studycalendar.web.admin.AdministerUserCommand"/>
+<jsp:useBean id="startingNewUser" scope="request"
+             type="java.lang.Boolean"/>
 
 <html>
 <head>
@@ -104,9 +107,11 @@
                 </c:otherwise>
             </c:choose>
         </div>
-        <div class="row">
-            <div class="value"><tags:errors path="*"/></div>
-        </div>
+        <c:if test="${not startingNewUser}">
+            <div class="row">
+                <div class="value"><tags:errors path="*"/></div>
+            </div>
+        </c:if>
         <div class="row">
             <div class="label">
                 <form:label path="user.csmUser.emailId">E-mail address</form:label>

web/src/test/java/edu/northwestern/bioinformatics/studycalendar/web/admin/AdministerUserCommandTest.java
@@ -337,7 +337,7 @@ public class AdministerUserCommandTest extends WebTestCase {
     public void testInvalidWhenBlankUsername() throws Exception {
         command.getUser().getCsmUser().setLoginName("\t");
         doValidate();
-        assertFieldErrorCount("user.loginName", 1);
+        assertFieldErrorCount("user.csmUser.loginName", 1);
     }
 
     public void testInvalidWhenNewInstanceWithDuplicateUsernameAndLookupNotConfigured() throws Exception {
@@ -351,7 +351,7 @@ public class AdministerUserCommandTest extends WebTestCase {
 
         doValidate();
 
-        assertFieldErrorCount("user.loginName", 1);
+        assertFieldErrorCount("user.csmUser.loginName", 1);
     }
 
     public void testValidWhenNewInstanceWithDuplicateUsernameAndLookupIsConfigured() throws Exception {
@@ -365,7 +365,7 @@ public class AdministerUserCommandTest extends WebTestCase {
 
         doValidate();
 
-        assertFieldErrorCount("user.loginName", 0);
+        assertFieldErrorCount("user.csmUser.loginName", 0);
     }
 
     public void testValidWhenDuplicateUsernameAndEditingSavedInstance() throws Exception {
@@ -374,7 +374,7 @@ public class AdministerUserCommandTest extends WebTestCase {
 
         doValidate();
 
-        assertFieldErrorCount("user.loginName", 0);
+        assertFieldErrorCount("user.csmUser.loginName", 0);
     }
 
     public void testInvalidWhenDuplicateUsernameAndEditingDifferentInstance() throws Exception {
@@ -386,28 +386,28 @@ public class AdministerUserCommandTest extends WebTestCase {
 
         doValidate();
 
-        assertFieldErrorCount("user.loginName", 1);
+        assertFieldErrorCount("user.csmUser.loginName", 1);
     }
 
     public void testInvalidWithBlankEmailAddress() throws Exception {
         command.getUser().getCsmUser().setEmailId("\n\t");
 
         doValidate();
-        assertFieldErrorCount("user.emailId", 1);
+        assertFieldErrorCount("user.csmUser.emailId", 1);
     }
 
     public void testInvalidWithNonAddressEmailAddress() throws Exception {
         command.getUser().getCsmUser().setEmailId("hello");
 
         doValidate();
-        assertFieldErrorCount("user.emailId", 1);
+        assertFieldErrorCount("user.csmUser.emailId", 1);
     }
 
     public void testValidWithReasonableEmailAddress() throws Exception {
         command.getUser().getCsmUser().setEmailId("someguy@nemo.it");
 
         doValidate();
-        assertFieldErrorCount("user.emailId", 0);
+        assertFieldErrorCount("user.csmUser.emailId", 0);
     }
 
     public void testPasswordRequiredForNewUserIfLocalPasswordsUsed() throws Exception {
@@ -456,28 +456,28 @@ public class AdministerUserCommandTest extends WebTestCase {
         command.getUser().getCsmUser().setFirstName(" ");
         
         doValidate();
-        assertFieldErrorCount("user.firstName", 1);
+        assertFieldErrorCount("user.csmUser.firstName", 1);
     }
     
     public void testSetFirstNameIsValid() throws Exception {
         command.getUser().getCsmUser().setFirstName("Jo");
         
         doValidate();
-        assertFieldErrorCount("user.firstName", 0);
+        assertFieldErrorCount("user.csmUser.firstName", 0);
     }
     
     public void testBlankLastNameIsInvalid() throws Exception {
         command.getUser().getCsmUser().setLastName(" ");
         
         doValidate();
-        assertFieldErrorCount("user.lastName", 1);
+        assertFieldErrorCount("user.csmUser.lastName", 1);
     }
     
     public void testSetLastNameIsValid() throws Exception {
         command.getUser().getCsmUser().setLastName("Jo");
         
         doValidate();
-        assertFieldErrorCount("user.lastName", 0);
+        assertFieldErrorCount("user.csmUser.lastName", 0);
     }
     
     private void assertFieldErrorCount(String field, int expectedCount) {